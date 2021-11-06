63de6ba394bbc06758cb988d35b86404c2b3925f
“[atefeh <[atefeh@buzzjective.com]>
2021-11-06 22:24:33 +0330

    add position title and email address

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
   <div id="app">
     <div class="area">
       <div class="intro heading-primary">
-        <h3 class="heading-primary-main">Atefeh Atashi</h3>
+        <h3 class="heading-primary-main">
+          Atefeh Atashi
+          <span class="heading-primary-main">Front-end Developer</span>
+        </h3>
         <div class="socials heading-primary-sub">
-          <a href="https://medium.com/@atefeh_sa93"><i class="fab fa-medium"></i></a>
-          <a href="https://twitter.com/atefeh_sa93"><i class="fab fa-twitter"></i></a>
-          <a href="https://github.com/atefeh-sa93"><i class="fab fa-github"></i></a>
-          <a href="https://codepen.io/atefeh_sa93"><i class="fab fa-codepen"></i></a>
+          <a href="https://medium.com/@atefeh_sa93"
+            ><i class="fab fa-medium"></i
+          ></a>
+          <a href="https://twitter.com/atefeh_sa93"
+            ><i class="fab fa-twitter"></i
+          ></a>
+          <a href="https://github.com/atefeh-sa93"
+            ><i class="fab fa-github"></i
+          ></a>
+          <a href="https://www.linkedin.com/in/atefeh-atashi/"
+            ><i class="fab fa-linkedin-in"></i
+          ></a>
+          <a href="mailto:ati.atashi1993@gmail.com"
+            ><i class="fas fa-envelope"></i
+          ></a>
         </div>
       </div>
       <ul class="circles">
